bc30cbf56df8f3c70f1ff22787b19327af418bd7
Chris Barry <chris@barry.im>
2013-09-14 13:22:45 -0400

    Grep was doing partial matches, actually works now.

--- vrms.sh
+++ vrms.sh
@@ -10,13 +10,13 @@ BAD="https://projects.parabolagnulinux.org/blacklist.git/plain/blacklist.txt";
 INSTALLED=$(mktemp);
 BLACKLIST=$(mktemp);
 
-pacman -Q > $INSTALLED;
+pacman -Qq > $INSTALLED;
 
 curl --silent $BAD | awk -F: '{ print $1 }' > $BLACKLIST;
 
 cat $BLACKLIST | while read I
 do
-	grep $I $INSTALLED;
+	grep -Fx --color=auto $I $INSTALLED;
 done
 
 rm $INSTALLED $BLACKLIST;
